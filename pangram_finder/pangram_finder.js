25fd21fdfb746c9db90e88c16ed22e4d846d9c6f
Ailsa Young <ailsa.young16@hotmail.co.uk>
2020-12-09 22:33:42 +0000

    isPangram method working

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,11 @@
 const PangramFinder = function (phrase) {
+  this.phrase = phrase
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
-}
+}                 
 
 PangramFinder.prototype.isPangram = function () {
-
+  const lowerPhrase = this.phrase.toLowerCase();
+  return this.alphabet.every(letter => lowerPhrase.includes(letter));
 }
 
 module.exports = PangramFinder;
